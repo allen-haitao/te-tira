c68c6639e42b4d9383645a871c3308bc533d20fd
yl882 <yl882@students.waikato.ac.nz>
2024-08-11 21:26:56 +1200

    update

--- frontend/src/components/search/Search.tsx
+++ frontend/src/components/search/Search.tsx
@@ -1,13 +1,39 @@
 import React from "react";
 import styles from "./Search.module.css";
-import {Input} from "antd";
-
+import { Typography, Input, Button } from "antd";
+import { EnvironmentOutlined, CalendarOutlined, UserOutlined } from "@ant-design/icons";
 
 export const Search: React.FC = () => {
     return (
-        <Input.Search
-        placeholder="Please enter a travel destination, theme, or keyword"
-        className={styles["search-input"]}
-        />
+        <div className={styles["search-container"]}>
+            <div className={styles["search-text"]}>
+                <Typography.Text className={styles["search-title"]}>
+                    Find your next stay
+                </Typography.Text>
+                <Typography.Text className={styles["search-subtitle"]}>
+                    Search low prices on hotels, homes and much more...
+                </Typography.Text>
+            </div>
+            <div className={styles["search-border"]}>
+                <Input
+                    prefix={<EnvironmentOutlined />}
+                    placeholder="Where are you going?"
+                    className={styles["search-input"]}
+                />
+                <Input
+                    prefix={<CalendarOutlined />}
+                    placeholder="Check-in date - Check-out date"
+                    className={styles["search-input"]}
+                />
+                <Input
+                    prefix={<UserOutlined />}
+                    placeholder="2 adults · 0 children · 1 room"
+                    className={styles["search-input"]}
+                />
+                <Button type="primary" className={styles["search-button"]}>
+                    Search
+                </Button>
+            </div>
+        </div>
     );
-}
+};
