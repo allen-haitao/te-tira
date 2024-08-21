25c5e0501dca61c92b9ce3706836725b6aafd6f8
yl882 <yl882@students.waikato.ac.nz>
2024-08-21 19:48:05 +1200

    update

--- frontend/src/assets/data/Array.tsx
+++ frontend/src/assets/data/Array.tsx
@@ -10,19 +10,3 @@ export const currencyItems = [
     { key: "8", label: "KRW", flag: "🇰🇷" },
     { key: "9", label: "TWD", flag: "🇹🇼" },
   ];
-
-export const langItems = ["English", "Māori", "简体中文", "繁體中文"];
-
-export const menuItems = [
-    { key: "1", label: "Hotel" },
-    { key: "2", label: "Motel" },
-    { key: "3", label: "Guesthouse" },
-    { key: "4", label: "B&B" },
-    { key: "5", label: "Resort" },
-    { key: "6", label: "Hostel" },
-    { key: "7", label: "Apartment" },
-    { key: "8", label: "Vacation Rental" },
-    { key: "9", label: "Lodge" },
-    { key: "10", label: "Inn" },
-    { key: "11", label: "Cabin" },
-  ];
