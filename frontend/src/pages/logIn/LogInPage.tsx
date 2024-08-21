a82e3ff54d9c060c2a7d3d146e0b5cd8064f9552
yl882 <yl882@students.waikato.ac.nz>
2024-08-21 18:48:17 +1200

    update

--- frontend/src/pages/logIn/LogInPage.tsx
+++ frontend/src/pages/logIn/LogInPage.tsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import React from "react";
+import { Header, Footer, Search, Carousel, SideMenu, Offer1, Offer2, ProductCollection, CountryRecommend, HotelRec} from "../../components";
+import { Row, Col, Typography, Button } from "antd";
+import styles from "./LogInPage.module.css";
+import { useTranslation } from "react-i18next";
 
 export const LogInPage:React.FC = () => {
-    return <h1>Log In</h1>
-};
+    
+    const { t } = useTranslation();
+    
+    return (
+      <>
+        <Header />
+        <div className={styles["page-content"]}>
+          <Row style={{ marginTop: 20 }}>
+            <h1>Log In</h1>
+          </Row>
+        </div>
+        <Footer />
+      </>
+    );
+  };
